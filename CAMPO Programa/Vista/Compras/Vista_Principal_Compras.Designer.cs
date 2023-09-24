8f1a8969a2feaa3ab8e135b8e898ac4fd1291a60
Zoel Villar <villar.zoel@gmail.com>
2023-09-24 20:35:10 -0300

    Bitacora Documentacion

--- CAMPO Programa/Vista/Compras/Vista_Principal_Compras.Designer.cs
+++ CAMPO Programa/Vista/Compras/Vista_Principal_Compras.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(476, 236);
+            this.ClientSize = new System.Drawing.Size(1050, 483);
             this.Controls.Add(this.label1);
             this.Name = "Vista_Principal_Compras";
             this.Text = "Form1";
